5d7fa615efc8578a95ce29dcb4d38237a2696f5a
mathias akari <cyphercode59@gmail.com>
2024-09-14 11:37:40 +0100

    new

---
+++ src/components/sections/contact/Addresses.tsx
@@ -0,0 +1,64 @@
+export default function Addresses() {
+  return (
+    <section>
+      <section>
+        <div>
+          <p className="text-nurakeemYellowColor font-medium text-lg">
+            Please know About Us
+          </p>
+          <h1 className="text-6xl font-bold mt-4 tracking-wide leading-tight font-lora">
+            Your Trusted Partner in Comprehensive Care and Quality Services
+          </h1>
+        </div>
+      </section>
+
+      <section>
+        <div className="flex-1">
+          <h2 className="text-customLightGreen text-[24px] font-lora font-semibold leading-[30.72px] text-left mb-4">
+            Our Services
+          </h2>
+          <div className="text-white">
+            <p>
+              <strong>LONDON / ESSEX:</strong> 6 Edgehill Gardens Dagenham RM10
+              7LB
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-1">
+          <h2 className="text-customLightGreen text-[24px] font-lora font-semibold leading-[30.72px] text-left mb-4">
+            24/7 Quick Contact
+          </h2>
+          <div className="text-white">
+            <p className="font-normal leading-[30px] text-left">
+              <strong>Phone</strong>020 3774 2766
+            </p>
+            <p className="font-normal leading-[30px] text-left mt-4">
+              Tel: 020 3774 2766
+              <br />
+              <span className="text-nurakeemYellowColor">
+                services@nurakeems.com
+              </span>
+              <br />
+              info@nurakeems.com
+            </p>
+          </div>
+          <div className="flex-1">
+            <h2 className="text-customLightGreen text-[24px] font-lora font-semibold leading-[30.72px] text-left mb-4">
+              Working Hours
+            </h2>
+            <p className="font-normal leading-[30px] text-left">
+              <strong>Mon - Fri:</strong> 9.00AM to 5.00PM
+            </p>
+            <p>
+              <strong>Saturday:</strong> 10.00AM to 5.00PM
+            </p>
+            <p>
+              <strong>Sunday:</strong> 12AM to 4.00PM
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+}
