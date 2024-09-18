ac8ac6e800bdae16133042adcf115649b4ddec8f
mathias akari <cyphercode59@gmail.com>
2024-09-18 17:33:22 +0100

    new

--- src/components/sections/services/serviceinfoCards.tsx
+++ src/components/sections/services/serviceinfoCards.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Container from "@/components/layout/wrapper/Container";
 import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
 
 const cards = [
   {
@@ -39,37 +41,182 @@ const cards = [
       "From planning events to providing entertainment options, we make sure your leisure time is enjoyable and memorable.",
   },
 ];
-
-export default function ServiceInfoCards() {
+const DesktopCards = () => {
   return (
-    <section className="mt-40 mb-40 w-full">
+    <section>
+      <div className="my-10 w-full border-t border-dashed border-formBorderColor" />
       <Container>
         <div className="grid grid-cols-2 gap-8 mobile_lg:grid-cols-1 tablet_min:grid-cols-2">
-          {cards.map((card, index) => (
-            <div
-              key={index}
-              className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden"
-            >
-              <div className="relative h-80">
-                <Image
-                  className="object-cover w-full h-full"
-                  src={card.img}
-                  alt={`${card.title} Image`}
-                  layout="fill"
-                  objectFit="cover" // Ensures proper scaling and cropping
-                  loading="lazy" // Lazy loading to improve performance
-                />
-              </div>
-              <div className="p-6">
-                <h5 className="text-xl font-semibold text-center text-primary mb-2">
-                  {card.title}
-                </h5>
-                <p className="text-center text-gray-600">{card.descr}</p>
-              </div>
+          <div className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden">
+            <div className="relative h-80">
+              <Image
+                className="object-cover w-full h-full"
+                src={cards[0].img}
+                alt={`${cards[0].title} Image`}
+                layout="fill"
+                objectFit="cover" // Ensures proper scaling and cropping
+                loading="lazy" // Lazy loading to improve performance
+              />
             </div>
-          ))}
+            <div className="p-6">
+              <h5 className="text-xl font-semibold text-center text-primary mb-2">
+                {cards[0].title}
+              </h5>
+              <p className="text-center text-primary">{cards[0].descr}</p>
+            </div>
+          </div>
+          <div className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden">
+            <div className="relative h-80">
+              <Image
+                className="object-cover w-full h-full"
+                src={cards[1].img}
+                alt={`${cards[1].title} Image`}
+                layout="fill"
+                objectFit="cover" // Ensures proper scaling and cropping
+                loading="lazy" // Lazy loading to improve performance
+              />
+            </div>
+            <div className="p-6">
+              <h5 className="text-xl font-semibold text-center text-primary mb-2">
+                {cards[1].title}
+              </h5>
+              <p className="text-center text-gray-600">{cards[1].descr}</p>
+            </div>
+          </div>
         </div>
       </Container>
+
+      <div className="my-10 w-full border-t border-dashed border-formBorderColor" />
+      <Container>
+        <div className="grid grid-cols-2 gap-8 mobile_lg:grid-cols-1 tablet_min:grid-cols-2">
+          <div className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden">
+            <div className="relative h-80">
+              <Image
+                className="object-cover w-full h-full"
+                src={cards[2].img}
+                alt={`${cards[2].title} Image`}
+                layout="fill"
+                objectFit="cover" // Ensures proper scaling and cropping
+                loading="lazy" // Lazy loading to improve performance
+              />
+            </div>
+            <div className="p-6">
+              <h5 className="text-xl font-semibold text-center text-primary mb-2">
+                {cards[2].title}
+              </h5>
+              <p className="text-center text-primary">{cards[2].descr}</p>
+            </div>
+          </div>
+          <div className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden">
+            <div className="relative h-80">
+              <Image
+                className="object-cover w-full h-full"
+                src={cards[3].img}
+                alt={`${cards[3].title} Image`}
+                layout="fill"
+                objectFit="cover" // Ensures proper scaling and cropping
+                loading="lazy" // Lazy loading to improve performance
+              />
+            </div>
+            <div className="p-6">
+              <h5 className="text-xl font-semibold text-center text-primary mb-2">
+                {cards[3].title}
+              </h5>
+              <p className="text-center text-gray-600">{cards[3].descr}</p>
+            </div>
+          </div>
+        </div>
+      </Container>
+
+      <div className="my-10 w-full border-t border-dashed border-formBorderColor" />
+      <Container>
+        <div className="grid grid-cols-2 gap-8 mobile_lg:grid-cols-1 tablet_min:grid-cols-2">
+          <div className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden">
+            <div className="relative h-80">
+              <Image
+                className="object-cover w-full h-full"
+                src={cards[4].img}
+                alt={`${cards[4].title} Image`}
+                layout="fill"
+                objectFit="cover" // Ensures proper scaling and cropping
+                loading="lazy" // Lazy loading to improve performance
+              />
+            </div>
+            <div className="p-6">
+              <h5 className="text-xl font-semibold text-center text-primary mb-2">
+                {cards[4].title}
+              </h5>
+              <p className="text-center text-primary">{cards[4].descr}</p>
+            </div>
+          </div>
+          <div className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden">
+            <div className="relative h-80">
+              <Image
+                className="object-cover w-full h-full"
+                src={cards[5].img}
+                alt={`${cards[5].title} Image`}
+                layout="fill"
+                objectFit="cover" // Ensures proper scaling and cropping
+                loading="lazy" // Lazy loading to improve performance
+              />
+            </div>
+            <div className="p-6">
+              <h5 className="text-xl font-semibold text-center text-primary mb-2">
+                {cards[5].title}
+              </h5>
+              <p className="text-center text-primary">{cards[5].descr}</p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+const MobileCards = () => {
+  return (
+    <section>
+      {[1, 2, 3, 4, 5, 6].map((card, index) => {
+        return (
+          <div key={index}>
+            <div className="my-10 w-full border-t border-dashed border-formBorderColor" />
+            <Container>
+              <div className="grid grid-cols-1 gap-8 mobile_lg:grid-cols-1 tablet_min:grid-cols-2">
+                <div className="max-w-[100%] mx-auto bg-white rounded-lg overflow-hidden">
+                  <div className="relative h-80">
+                    <Image
+                      className="object-cover w-full h-full"
+                      src={cards[index].img}
+                      alt={`${cards[index].title} Image`}
+                      layout="fill"
+                      objectFit="cover" // Ensures proper scaling and cropping
+                      loading="lazy" // Lazy loading to improve performance
+                    />
+                  </div>
+                  <div className="p-6">
+                    <h5 className="text-xl font-semibold text-center text-primary mb-2">
+                      {cards[index].title}
+                    </h5>
+                    <p className="text-center text-primary">
+                      {cards[index].descr}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Container>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default function ServiceInfoCards() {
+  const isDesktop = useMediaQuery({ query: "(min-width: 768px)" });
+
+  return (
+    <section className="mt-40 mb-40 w-full">
+      {isDesktop ? <DesktopCards /> : <MobileCards />}
     </section>
   );
 }
